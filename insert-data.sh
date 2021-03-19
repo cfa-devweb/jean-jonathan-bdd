0a3a59994adc3bfded326df70517af664da9ff17
Jean Masso <68371410+jeanmasso@users.noreply.github.com>
2021-03-19 13:15:17 +1100

    save

--- insert-data.sh
+++ insert-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mysql --local-infile=1 --user=root --host=localhost  -e "USE library; LOAD DATA LOCAL INFILE 'books.csv' INTO TABLE authors FIELDS TERMINATED BY ',' ENCLOSED BY '\"' (@Title, @Auhthor) SET Name = @Author;" -proot
-# mysql --local-infile=1 --user=root --host=localhost  -e "USE library; LOAD DATA LOCAL INFILE 'books.csv' INTO TABLE genres FIELDS TERMINATED BY ',' (@Title, @Auhthor, @Genre) SET Name = @Genre;" -proot
-# mysql --local-infile=1 --user=root --host=localhost  -e "USE library; LOAD DATA LOCAL INFILE 'books.csv' INTO TABLE books FIELDS TERMINATED BY ',' (@Title, @Genre, @Height) SET Title = @Title, Height = @Height; SET Genre = (SELECT id FROM genres WHERE name = @Genre)" -proot
+mysql --local-infile=1 --user=root --host=localhost  -e "USE library; LOAD DATA LOCAL INFILE 'books.csv' INTO TABLE authors FIELDS TERMINATED BY ',' ENCLOSED BY '\"' IGNORE 1 LINES (@Title, @Author) SET Name = @Author;" -proot
+mysql --local-infile=1 --user=root --host=localhost  -e "USE library; LOAD DATA LOCAL INFILE 'books.csv' INTO TABLE books FIELDS TERMINATED BY ',' ENCLOSED BY '\"' IGNORE 1 LINES (@Title, @Author, @Genre, @Height, @Publisher) SET Title = @Title, Genre = @Genre, Height = @Height, Publisher = @Publisher;" -proot
+# mysql --local-infile=1 --user=root --host=localhost  -e "USE library; LOAD DATA LOCAL INFILE 'books-parse.csv' INTO TABLE books FIELDS TERMINATED BY ',' (@Title, @Genre, @Height) SET Title = @Title, Height = @Height; SET Genre = (SELECT id FROM genres WHERE name = @Genre)" -proot
 
 echo "Executed command"
--- library.sql
+++ library.sql
@@ -6,58 +6,39 @@ Use library;
 
 drop table if exists clients;
 
-create table clients
+create table clients 
 (
     id int primary key auto_increment not null,
     FirstName varchar(25) not null,
     LastName varchar(25) not null,
     Birthday date,
-    Phone varchar(10) not null unique,
-    Mail varchar(40) not null unique
+    Phone varchar (10) not null unique,
+    Mail varchar (40) not null unique
 );
 
-drop table if exists genres;
+drop table if exists authors;
 
-create table genres
+create table authors 
 (
     id int primary key auto_increment not null,
-    Name varchar(25) not null unique
-);
-
-drop table if exists publishers ;
-
-create table publishers
-(
-    id int primary key auto_increment not null,
-    Name varchar(25) unique
-);
-
-drop table if exists authors ;
-
-create table authors
-(
-    id int primary key auto_increment not null,
-    Name varchar(30),
+    Name varchar (25),
     Birthday date not null
 );
 
-drop table if exists books;
+drop table if exists books ;
 
-create table books
+create table books 
 (
     id int primary key auto_increment not null,
     Title varchar(75) not null unique,
     Height int(4),
-    Genre int default null,
-    Publisher int default null,
+    Genre varchar(25) default null,
+    Publisher varchar(25) default null,
     Author int default null,
-    constraint books_genres_id foreign key (Genre) references genres (id) on delete cascade,
-    constraint books_publishers_id foreign key (Publisher) references publishers (id) on delete cascade,
     constraint books_authors_id foreign key (Author) references authors (id) on delete cascade
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+)engine=InnoDB default charset=utf8 collate=utf8_general_ci;
 
-
-drop table if exists registrations;
+drop table if exists registrations; 
 
 create table registrations
 (
@@ -68,10 +49,4 @@ create table registrations
     client int default null,
     constraint registrations_books_id foreign key (Book) references books (id) on delete cascade,
     constraint registrations_clients_id foreign key (Client) references clients (id) on delete cascade
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
-
-
-
-
+)engine=InnoDB default charset=utf8 collate=utf8_general_ci;
